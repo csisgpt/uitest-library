efce4986ebcb94e494ada8ef0d63c3326f77345a
csisgpt <csisgpt@gmail.com>
2025-07-28 10:22:02 +0330

    delete to start project again

--- ui-library/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseButton } from './components/BaseButton.vue'
-export { default as BaseInput } from './components/BaseInput.vue'
-export { default as BaseModal } from './components/BaseModal.vue'
-export { default as BaseTable } from './components/BaseTable.vue'
