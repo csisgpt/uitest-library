0c3035c52ea76c81680488b237006379db3dfd51
csisgpt <csisgpt@gmail.com>
2025-07-28 10:33:26 +0330

    Remove unintended compiled files

---
+++ ui-library/components/BaseButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button :class="$style.button" type="button">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style module src="./BaseButton.module.css"></style>
---
+++ ui-library/demo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <BaseButton @click="toggle">Toggle Theme</BaseButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const current = ref<'light' | 'dark'>('light');
+
+function toggle() {
+  current.value = current.value === 'light' ? 'dark' : 'light';
+  document.body.setAttribute('data-theme', current.value);
+}
+</script>
