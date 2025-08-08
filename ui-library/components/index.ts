daaf48f523a6ea4cec09e5aa6dc8bb0541cf42e0
csisgpt <csisgpt@gmail.com>
2025-08-09 00:18:35 +0330

    feat: add base data table component

--- ui-library/components/index.ts
+++ ui-library/components/index.ts
@@ -19,3 +19,4 @@ export { default as BaseSplitButton } from './BaseSplitButton/BaseSplitButton.vu
 export { default as BaseSpeedDial } from './BaseSpeedDial/BaseSpeedDial.vue';
 export { default as BaseImage } from './BaseImage/BaseImage.vue';
 export { default as BaseIcon } from './BaseIcon/BaseIcon.vue';
+export { default as BaseDataTable } from './BaseDataTable/BaseDataTable.vue';
